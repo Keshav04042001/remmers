85b64f4a053150a75355cfd4035dbf849d7b92bc
Anirudh Soni <anirudh.soni007@gmail.com>
2021-02-20 20:54:45 +0530

    update footer

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-    $(".nav-bar").load("./web/nav.html")
+    $(".nav-bar").load("./web/nav.html");
+    $(".footer").load("./web/footer.html);
 
     $("#pricing").click(function(){
       $(".content").load("./web/pricing.html");
